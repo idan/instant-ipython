2895cc9bb72e1129e7f92e1e2ec0d541b05b6d53
Idan Gazit <idan@gazit.me>
2015-01-28 11:38:09 +0200

    Remove sourcing of .profile.d/python.sh
    We're using conda, this doesn't exist

--- .profile.d/ipython.sh
+++ .profile.d/ipython.sh
@@ -1,5 +1,3 @@
-source '/app/.profile.d/python.sh'
-
 if [[ -n "${IPYTHON_PASSWORD:+_}" ]]; then
 	export IPYTHON_PASSWORD_HASH=$( python -c "from IPython.lib import passwd; print passwd('${IPYTHON_PASSWORD}')" )
 fi
